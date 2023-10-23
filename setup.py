8f9d7bc960d3ebddd77ab480086b340e42d5584e
sambhavm22 <58766591+sambhavm22@users.noreply.github.com>
2023-10-23 23:09:48 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'WasteDetection',
+    version= '0.0.0',
+    author= 'Sambhav Mehta',
+    author_email= 'sambhavm22@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
