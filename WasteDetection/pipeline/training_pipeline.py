a8678633ff5a46cffb934489f89910af636d5142
sambhavm22 <58766591+sambhavm22@users.noreply.github.com>
2023-10-24 23:45:33 +0200

    data validation process completed

--- WasteDetection/pipeline/training_pipeline.py
+++ WasteDetection/pipeline/training_pipeline.py
@@ -2,13 +2,15 @@ import os, sys
 from WasteDetection.logger import logging
 from WasteDetection.exception import AppException
 from WasteDetection.components.data_ingestion import DataIngestion
+from WasteDetection.components.data_validation import DataValidation
 
-from WasteDetection.entity.config_entity import DataIngestionConfig
-from WasteDetection.entity.artifacts_entity import DataIngestionArtifact
+from WasteDetection.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from WasteDetection.entity.artifacts_entity import DataIngestionArtifact,DataValidationArtifact
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
@@ -28,10 +30,33 @@ class TrainPipeline:
         except Exception as e:
             raise AppException(e, sys)
         
+    def start_data_validation(self,
+                            data_ingestion_artifact: DataIngestionConfig) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact = data_ingestion_artifact,
+                data_validation_config = self.data_validation_config
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Performed the data validation operation")
+
+            logging.info("Exited the start_data_validation method of TrainPipeline class")
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
+        
 
     def run_pipeline(self)->None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact = data_ingestion_artifact
+            )
             
 
         except Exception as e:
